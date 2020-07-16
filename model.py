89df75b66d23bbab4be4d0978490e75d16659667
Tom Wyllie <wyllietom@gmail.com>
2020-07-17 00:29:53 +0100

    Adjust model and training params

--- model.py
+++ model.py
@@ -9,8 +9,6 @@ def vgg_style(input_tensor):
     The original feature extraction structure from CRNN paper.
     Related paper: https://ieeexplore.ieee.org/abstract/document/7801919
     """
-    # return input_tensor
-    # TODO change strides...
     x = layers.Conv2D(8, 3, padding='same', activation='relu')(input_tensor)
     x = layers.MaxPool2D(pool_size=2, padding='same')(x)
 
@@ -42,6 +40,6 @@ def build_model(image_width=None, channels=1):
     x = vgg_style(img_input)
     x = layers.Reshape((-1, 64))(x)
 
-    x = layers.Bidirectional(layers.LSTM(units=55, return_sequences=True))(x)
+    x = layers.Bidirectional(layers.LSTM(units=64, return_sequences=True))(x)
     x = layers.Dense(units=config.NUM_CLASSES)(x)
     return keras.Model(inputs=img_input, outputs=x, name='CRNN')
